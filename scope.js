15749dd29e8d606638e52ac0e726fd165992321e
Joe Versoza <jversoza@xsmail.com>
2010-12-02 21:31:43 -0500

    started adding code for function scope

--- scope.js
+++ scope.js
@@ -9,4 +9,32 @@ var ejs = {
 			}
 		}
 };
-ejs.debug("hello");
+
+ejs.debug('variables defined within a function are out of scope for functions called within that function');
+var v = "global var";
+
+function functionOne() {
+	ejs.debug('calling functionOne');
+	ejs.debug(v);
+}
+
+function functionTwo() {
+	var v = "f2";
+	ejs.debug('v as defined in functionTwo: ' + v);
+	ejs.debug('calling functionTwo');
+	functionOne();
+}
+
+functionTwo();
+
+ejs.debug('variables defined within a function are in scope for functions that are defined in the parent function');
+function functionTwo() {
+	var v = "f2";
+	ejs.debug('v as defined in functionTwo: ' + v);
+	ejs.debug('calling functionTwo');
+	function functionOne() {
+		ejs.debug('calling functionOne');
+		ejs.debug(v);
+	}
+	functionOne();
+}
