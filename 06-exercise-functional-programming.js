45bd25c15b9f36c84181badc6bcf46238e38b7f1
Joe Versoza <jversoza@xsmail.com>
2011-01-19 09:52:10 -0500

    compiled commonly used functions... need to figure out how to just do import in rhino

--- 06-exercise-functional-programming.js
+++ 06-exercise-functional-programming.js
@@ -747,7 +747,7 @@ ejs.debug(map(partial(map, square), [[10, 100], [12, 16], [0,1]]));
 ejs.banner('function composition');
 function compose(f1, f2) {
 	return function () {
-		return f1(f2.apply(null,arguments));
+		return !f1(f2.apply(null,arguments));
 	}
 }
 var isUndefined = partial(op['==='], undefined);
@@ -755,3 +755,53 @@ var isDefined = compose(op['!'], isUndefined);
 ejs.debug(isDefined(Math.PI));
 ejs.debug(isDefined(Math.PIE));
 ejs.debug(isUndefined(Math.PI));
+/*
+Commonly used functions
+
+function forEach(a, f) {
+	for (i in a)
+		f(a[i]);
+}
+function map(f, a) {
+	var result = []
+	forEach(a, function(e) {
+		result.push(f(e));
+	}); 
+	return result;
+}
+function reduce(combine, base, a) {
+	forEach(a, function(x) {
+		base = combine(base, x);
+	});
+	return base;
+}
+var op = {
+	'+': function(a, b) {return a + b;},
+	'-': function(a, b) {return a - b;},
+	'*': function(a, b) {return a * b;},
+	'/': function(a, b) {return a / b;},
+	'==': function(a, b) {return a == b;},
+	'===': function(a, b) {return a === b;},
+	'!': function(a) {return !a;}
+}
+function asArray(quasiArray, start) {
+	var result = [];
+	for (var i=(start || 0); i < quasiArray.length; i++)
+		result.push(quasiArray[i]);
+	return result;
+}
+
+function partial(func) {
+	// need to stored fixed args in closure
+	var fixedArgs = asArray(arguments, 1);
+	return function() {
+		// when a new function is returned, it will always have fixedArgs as its first arg
+		return func.apply(null, fixedArgs.concat(asArray(arguments)));
+	};
+}
+function compose(f1, f2) {
+	return function () {
+		return !f1(f2.apply(null,arguments));
+	}
+}
+*/
